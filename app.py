a3638f182079c6579a68ed2a491bf9c74079ab07
Yoodahun <dahun4032@gmail.com>
2021-01-13 00:11:29 +0900

    ADD resources

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+items = []
+
+
+class Item(Resource):
+    def get(self, name):
+
+        for item in items:
+            if item["name"] == name:
+                return item
+
+        return {'item': None}, 404
+
+    def post(self, name):
+        jsonPayload = request.get_json(force=True)
+        item = {'name': name, 'price': jsonPayload['price']}
+        items.append(item)
+
+        return item, 201  # create success
+
+    def put(self, name):
+        pass
+
+    def delete(self, name):
+        pass
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
