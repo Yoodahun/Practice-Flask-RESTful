80608284782633632ff4b542fb9be79eb6335343
Yoodahun <dahun4032@gmail.com>
2021-01-20 23:43:41 +0900

    UPDATE initialize database sql-alchemy

--- src/app.py
+++ src/app.py
@@ -5,8 +5,10 @@ from flask_jwt import JWT
 from security import authenticate, identify
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from db import db
 
 app = Flask(__name__) #MUST
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #turns off modification tracker
 app.secret_key = 'jose'
 api = Api(app) # MUST
 
@@ -17,4 +19,5 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
