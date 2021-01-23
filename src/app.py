917ee5e12ecffa3420d0a052092f36c58c7e6d7c
Yoodahun <dahun4032@gmail.com>
2021-01-23 16:38:19 +0900

    ADD store resources and importing in app.py

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ from flask_jwt import JWT
 from security import authenticate, identify
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__) #MUST
@@ -22,6 +23,9 @@ def create_tables():
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+
 
 if __name__ == '__main__':
     db.init_app(app)
