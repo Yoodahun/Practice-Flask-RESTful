833691834e128a1510c5560b065f2d169809ec67
Yoodahun <dahun4032@gmail.com>
2021-01-31 00:31:00 +0900

    ADD UserLogin class using flask-jwt-extended

--- src/resources/user.py
+++ src/resources/user.py
@@ -1,23 +1,25 @@
 from flask_restful import Resource, reqparse
 from src.models.user import UserModel
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username',
+                          type=str,
+                          required=True,
+                          help="This field cannot be lef blank."
+                          )
+_user_parser.add_argument('password',
+                          type=str,
+                          required=True,
+                          help="This field cannot be lef blank."
+                          )
 
 
 class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-                        type=str,
-                        required=True,
-                        help="This field cannot be lef blank."
-                        )
-    parser.add_argument('password',
-                        type=str,
-                        required=True,
-                        help="This field cannot be lef blank."
-                        )
 
     def post(self):
-
-        data = UserRegister.parser.parse_args()
+        data = _user_parser.parse_args()
 
         if UserModel.find_by_username(data['username']):
             return {"message": "A User with that username already exists"}, 400
@@ -34,7 +36,7 @@ class User(Resource):
     def get(cls, user_id):
         user = UserModel.find_by_id(user_id)
         if not user:
-            return {'message':'User not found'}, 404
+            return {'message': 'User not found'}, 404
 
         return user.json()
 
@@ -45,4 +47,29 @@ class User(Resource):
             return {'message': 'User not found'}, 404
 
         user.delete_from_db()
-        return {'message':'User deleted successfully.'}, 200
+        return {'message': 'User deleted successfully.'}, 200
+
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        # get data from parser
+        data = _user_parser.parse_args()
+
+        # find user in DB
+        user = UserModel.find_by_username(data['username'])
+
+        # check password
+        if user and safe_str_cmp(user.password, data['password']):
+            # create access token
+            access_token = create_access_token(identity=user.id, fresh=True)
+            # create refresh token
+            refresh_token = create_refresh_token(user.id)
+            # return
+            return {
+                       'access_token': access_token,
+                       'refresh_token': refresh_token
+                   }, 200
+
+        return {'message': 'Invalid credentials'}, 401
