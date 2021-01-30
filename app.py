dba821a0e6d58e24d8f80114a02958eb6d27577e
Yoodahun <dahun4032@gmail.com>
2021-01-31 00:31:39 +0900

    ADD flask_jwt_extended for JWTManager ADD end-point for UserLogin

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 
 from src.security import authenticate, identify
-from src.resources.user import UserRegister, User
+from src.resources.user import UserRegister, User, UserLogin
 from src.resources.item import Item, ItemList
 from src.resources.store import Store, StoreList
 from src.db import db
@@ -17,12 +17,14 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app) # MUST
 
-jwt = JWT(app, authenticate, identify)  # /auth
+# jwt = JWT(app, authenticate, identify)  # /auth
+jwt = JWTManager(app) # not creating end-point /auth
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
