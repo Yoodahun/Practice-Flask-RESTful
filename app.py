45f64a6846873a03023486200cd50514f5cd3f40
Yoodahun <dahun4032@gmail.com>
2021-01-31 17:54:19 +0900

    ADD jwt decorator

--- app.py
+++ app.py
@@ -3,33 +3,73 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-
-from src.security import authenticate, identify
 from src.resources.user import UserRegister, User, UserLogin, TokenRefresh
 from src.resources.item import Item, ItemList
 from src.resources.store import Store, StoreList
 from src.db import db
 
-app = Flask(__name__) #MUST
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') # data.db is in root folder
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #turns off modification tracker
+app = Flask(__name__)  # MUST
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')  # data.db is in root folder
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # turns off modification tracker
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'jose' # app.config['JWT_SECRET_KEY']
-api = Api(app) # MUST
+app.secret_key = 'jose'  # app.config['JWT_SECRET_KEY']
+api = Api(app)  # MUST
 
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
+
 # jwt = JWT(app, authenticate, identify)  # /auth
-jwt = JWTManager(app) # not creating end-point /auth
+jwt = JWTManager(app)  # not creating end-point /auth
+
 
 @jwt.user_claims_loader
-def add_claims_to_jwt(identity):
-    if identity == 1: # Instead of hard-coding, should read from a config file or DB
+def add_claims_to_jwt(identity):  # connect to ItemList get method
+    if identity == 1:  # Instead of hard-coding, should read from a config file or DB
         return {'is_admin': True}
-    return {'is_admin':False}
+    return {'is_admin': False}
+
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return Flask.jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return Flask.jsonify({
+        'description': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return Flask.jsonify({
+        'description': 'Request does not contain an access token.',
+        'error': 'authorization_required'
+    }), 401
+
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback(error):
+    return Flask.jsonify({
+        'description': 'The token is not fresh',
+        'error': 'fresh_token_required'
+    }), 401
+
+
+@jwt.revoked_token_loader
+def revoked_token_callback(error):
+    return Flask.jsonify({
+        'description': 'The token has been revoked',
+        'error': 'token_revoked'
+    }), 401
 
 
 api.add_resource(Item, '/item/<string:name>')
@@ -40,7 +80,6 @@ api.add_resource(UserLogin, '/login')
 api.add_resource(TokenRefresh, '/refresh')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
-
 
 if __name__ == '__main__':
     db.init_app(app)
