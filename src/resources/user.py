9208802c62c6ef9c0c1e13a7dae932300b638aea
Yoodahun <dahun4032@gmail.com>
2021-01-31 16:40:56 +0900

    ADD TokenRefresh

--- src/resources/user.py
+++ src/resources/user.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource, reqparse
 from src.models.user import UserModel
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import *
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('username',
@@ -73,3 +73,11 @@ class UserLogin(Resource):
                    }, 200
 
         return {'message': 'Invalid credentials'}, 401
+
+
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_token': new_token}, 200
